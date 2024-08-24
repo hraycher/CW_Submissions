36a35e973fdb594f90e34648d8c49421c6edbb13
hraycher <hauraycher@gmail.com>
2024-08-23 15:24:33 -1000

    Update and rename Practicing System and Primitives to PracticingSystemandPrimitives.java

---
+++ PracticingSystemandPrimitives.java
@@ -0,0 +1,27 @@
+/*
+* This code is for the very first assignment.
+* It does two simple prints and then I submit it.
+*/
+public class Hello_World {
+	public static void main (String[]args) {
+		
+			System.out.println("0    0        000                000");
+			System.out.println("0    0       0   0              0   0");
+			System.out.println("0    0      0     0            0     0");
+			System.out.println("000000     000000000          000000000");
+			System.out.println("0    0    0         0        0         0");
+			System.out.println("0    0   0           0      0           0");
+	
+	
+	System.out.println((6.0*3.5-1.5*5)/(55.6-30.2));
+	final double pi=3.14159;
+	double r=4.0;
+	double c=pi*r*2;
+	System.out.println("whats the circumference?Here:       "+c);
+	double rSq=r*r;
+	double a=pi*rSq;
+	System.out.print("whats the area?Here:    "+a);
+	
+	}
+	
+}
