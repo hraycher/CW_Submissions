7723f3620a18bf48dd9f8e34281a0f1ed0b06159
hraycher <hauraycher@gmail.com>
2024-08-30 13:08:15 -1000

    Update Intro_Cube.java

--- Intro_Cube.java
+++ Intro_Cube.java
@@ -1,12 +1,21 @@
-
+/*
+ * This the computer science class this code gives the surface area
+ * of the cube and the volume of the cube my name is Ha'aheo thats all
+ * it does if you change the side length it will also change the surface
+ * area and the volume of the cube.
+ */
 public class Intro_Cube {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 double sl =6;
+//This is the side length of the cube//
 int surfacearea =(int) (6*sl*sl);
+//This is the Surface area of the cube//
 int vol = (int) (sl*sl*sl);
+//This is the volume of the cube//
 System.out.print("The sidelength of the cube is "+sl+ " the area is "+surfacearea+" the volume is "+vol);
+//This will show the surface area and the volume of the cube//
 	}
 
 }
