ac1dd804e6e3fad7a53c8b3f6303067f4dcf430f
hraycher <hauraycher@gmail.com>
2024-08-30 12:57:54 -1000

    Create Intro_Cube.java

---
+++ Intro_Cube.java
@@ -0,0 +1,12 @@
+
+public class Intro_Cube {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+double sl =6;
+int surfacearea =(int) (6*sl*sl);
+int vol = (int) (sl*sl*sl);
+System.out.print("The sidelength of the cube is "+sl+ " the area is "+surfacearea+" the volume is "+vol);
+	}
+
+}
