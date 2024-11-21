9cbf336f8144447db523633ed748ad575cfb9f95
hraycher <hauraycher@gmail.com>
2024-11-20 22:11:57 -1000

    Update logic1and2.java

--- logic1and2.java
+++ logic1and2.java
@@ -1,85 +1,102 @@
 public boolean cigarParty(int cigars, boolean isWeekend) {
- if (isWeekend == true && cigars >= 40)
- return true;
- if(!(isWeekend) && cigars >= 40 && cigars <= 60)
- return true;
+ if(!isWeekend){
+  return cigars >= 40 && cigars <= 60;
+ }
  else
- return false;
-
-}
+ {
+  return cigars >= 40;
+ }
+ }
 public int dateFashion(int you, int date) {
-  if(you <= 2 || date <= 2)
+if(you <= 2 || 2 >= date){
   return 0;
-  if(you>= 8 || date >= 8)
+}
+else
+{ 
+if(you >= 9 || 9 <= date){
   return 2;
-  else
+}
+else
+{
   return 1;
+  
+}
+}
 }
 public boolean squirrelPlay(int temp, boolean isSummer) {
-  if(isSummer && 60 <= temp && temp <=100)
-  return true;
-  if (!isSummer && 60 <= temp && temp <=90)
-  return true;
-  
-  return false;
+ if(isSummer == true){
+  return temp >= 60 &&  temp <= 100;
+ }
+ else{
+return temp >= 60 &&  temp <= 90;
 }
+}
+
+
 
 public int caughtSpeeding(int speed, boolean isBirthday) {
- if(isBirthday) {
-if(speed <= 65) {
-return 0;
- } else if (66 <= speed && speed <= 85){
-return 1;
-}else if (86 <= speed){
-return 2;
- }
- } 
- if (speed <= 60){
-   return 0;
- }
- else if(61 <= speed && speed<=80){
-   return 1;
- }else { 
-  return 2;
- }
+ if(isBirthday==false) {
+        if(speed <= 60) {
+            return 0;
+        } else if(speed <= 80) {
+            return 1;
+        } else {
+            return 2;
+        }
+    }
+    if(speed <= 65) {
+        return 0;
+    } else if(speed <= 85) {
+        return 1;
+    } else {
+        return 2;
+    }
 }
 public int sortaSum(int a, int b) {
-  int sum  = a+b;
-  if( 10<= sum && sum<=19)
-  return 20;
-  return sum;
+if( 10 <= a+b && a+b <= 19)
+return 20;
+return a+b;
+
 }
 public String alarmClock(int day, boolean vacation) {
-if(vacation){
-  if( day > 0 && day != 6)
-  return "10:00";
-  else 
-  return "off";
-}
-else if ( day>0 && day !=6)
+ if(vacation==false){
+   if (day >= 1 && day <= 5) {
 return "7:00";
-else 
-return "10:00";
-}
+  } else{
+    return "10:00";
+  }
+  }else{
+    if (day>=1 && day <= 5){
+      return "10:00";
+    }else{
+      return "off";
+    }
+  }
+ }
 public boolean in1To10(int n, boolean outsideMode) {
-if(!outsideMode){
-  if (n >= 1 && n <= 10){
+if(outsideMode){
+  if (n <= 1 || 10 <= n){
     return true;
   }
-  else{
-    return false;
+ } else
+  {
+    if(n>=1 && 10>=n){
+      return true;
+    }
   }
-}
-else {
-  if (n <= 1 || n >= 10){
-    return true;
-  }
-  else{
-    return false;
-  }
-}
-}
+ 
 
+return false;
+}
 public boolean old35(int n) {
-  return ( n % 3 == 0) != (n % 5 == 0);
+ if(n%3==0||n%5==0){
+  if(!(n%3==0&&n%5==0)) {
+    return true;
+  }else{
+    return false;
+  }
+}
+else
+return false;
+  
 }
