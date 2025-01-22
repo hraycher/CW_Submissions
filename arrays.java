5a133e216762c900aa67272e7f2562a71ed93e0f
hraycher <hauraycher@gmail.com>
2025-01-22 09:31:54 -1000

    Create arrays.java

---
+++ arrays.java
@@ -0,0 +1,28 @@
+
+
+public class tester {
+	/**
+	 * here is the questions you wanted me to answer.
+	 * @param args
+	 */
+	public static void main(String[] args) {
+   int [] numArr={7, -12, 88, -88, 0, 5, 2000};
+   double[]doubArr;
+   String[] fruitsArr= new String [5];
+	//here is the length of fruitsArr were basically it prints out 5 because thats the length
+  
+   fruitsArr[1]="Bapple";
+   
+   fruitsArr[fruitsArr.length-1]="Zanana";
+   
+   System.out.println(fruitsArr[1]);
+   System.out.println();
+   
+   
+   System.out.println(fruitsArr);
+	for(int i=0; i<fruitsArr.length;i++) {
+		System.out.println(i);
+	}
+	}
+
+  }
