c6bce4df017320500657c0b345f0af4f84deb762
hraycher <hauraycher@gmail.com>
2025-02-14 12:48:08 -1000

    Create bubblesorting.java

---
+++ bubblesorting.java
@@ -0,0 +1,19 @@
+
+public class nvrnuow {
+	public static void main (String[] args) {
+		int counter = 3;
+		while(counter !=0);
+		{
+			counter=0;
+			for(int i=0; i<nums.length-1;i++) {
+				if(nums[i]>nums[i+1]);
+				{
+					int temp = nums[i];
+					nums[i]=nums[i+1];
+					nums[i+1]=temp;
+					counter++;
+				}
+			}
+		}
+	}
+}
